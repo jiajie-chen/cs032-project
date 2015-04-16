d05081ffe6bddfb9eff1dc6d9c7030cfd74364ba
Jiajie Chen <jiajie_chen@brown.edu>
2015-04-16 18:28:26 -0400

    started DB

--- src/main/java/edu/brown/cs/jc124/database/AssetManager.java
+++ src/main/java/edu/brown/cs/jc124/database/AssetManager.java
@@ -1,5 +1,13 @@
 package edu.brown.cs.jc124.database;
 
 public class AssetManager {
-
+  private BookDatabase bd;
+  
+  public TextIterator getByAuthor() {
+    
+  }
+  
+  public TextIterator getByFacet() {
+    
+  }
 }
--- src/main/java/edu/brown/cs/jc124/database/BookDatabase.java
+++ src/main/java/edu/brown/cs/jc124/database/BookDatabase.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Set;
 
 public class BookDatabase {
   private Connection conn;
