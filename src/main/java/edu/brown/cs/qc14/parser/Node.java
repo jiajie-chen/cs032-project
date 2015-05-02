d32fac22c8cb8b803aa6edb085ba679ccd96bfd8
qihengchen <qihengchen@163.com>
2015-05-02 17:59:06 -0400

    remove underscores in Node

--- src/main/java/edu/brown/cs/qc14/parser/Node.java
+++ src/main/java/edu/brown/cs/qc14/parser/Node.java
@@ -5,24 +5,24 @@ import java.util.Arrays;
 
 public class Node {
 	
-	private String _parent;
-	private String _name;
-	private ArrayList<Node> _children;
+	private String parent;
+	private String name;
+	private ArrayList<Node> children;
 	
 	public Node(String name, String parent) {
-		_name = name;
-		_parent = parent;
+		name = name;
+		parent = parent;
 	}
 	
 	public Node(String name, String parent, Node left) {
-		_name = name;
-		_parent = parent;
-		_children = new ArrayList<Node>(Arrays.asList(left));
+		name = name;
+		parent = parent;
+		children = new ArrayList<Node>(Arrays.asList(left));
 	}
 	
 	public Node(String name, String parent, Node left, Node right) {
-		_name = name;
-		_parent = parent;
-		_children = new ArrayList<Node>(Arrays.asList(left, right));
+		name = name;
+		parent = parent;
+		children = new ArrayList<Node>(Arrays.asList(left, right));
 	}
 }
